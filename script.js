58b61ca69bdb42340b49f2651fae82408c80aae6
Cristian Correa <artudvels@gmail.com>
2021-09-21 13:17:03 -0300

    Actauliz, finalizando

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Holaaa");
+alert("5+5");
+
+for(var i = 0; i < 5; i++){
+    alert(i);
+}
+
+var bigDecimal = Math.random();
+var aleatorio = (bigDecimal*6)+1;
+var diceNum = Math.floor(aleatorio);
+
+alert(diceNum);
